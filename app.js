21ca34fff7e794ee9fb24826dc62dd73923a5b7b
Joseph Stocks <jpstocks63@gmail.com>
2021-01-12 09:25:09 -0600

    mostly done with forum page styling, needs more work on displayed comments

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static("public"));
 
 //routes
 app.use(require("./routes"));
+app.use(require("./routes/forum"));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}..`);
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
                 sans:
                     'Roboto, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
             },
+            width: {
+                md: "28rem",
+                lg: "32rem",
+                xl: "36rem",
+                "2xl": "42rem",
+                "3xl": "48rem",
+                "4xl": "56rem",
+                "5xl": "64rem",
+                "6xl": "72rem",
+                "7xl": "80rem",
+            },
         },
     },
     variants: {
