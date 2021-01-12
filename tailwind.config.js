a279def03a3f42161622f4c26e36d5a838031b9e
Joseph Stocks <jpstocks63@gmail.com>
2021-01-11 18:27:09 -0600

    have a working homescreen: NOT responsive at this time - good for large monitors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                logo: ['"Rock Salt"', "cursive"],
+                sans:
+                    'Roboto, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+            },
+        },
+    },
+    variants: {
+        extend: {
+            // backgroundColor: ["active"],
+        },
+    },
+    plugins: [],
+};
